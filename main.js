536cabf69d40be72a70ac1e1e9e1040a797a830c
unknown <radman.siddiki@gmail.com>
2020-07-31 12:46:25 +0600

    Prepare iframe and styling

---
+++ main.js
@@ -0,0 +1,1 @@
+document.getElementById("project").src="https://snap.berkeley.edu/embed?project=" + document.getElementById("Username").value + "&user="+ document.getElementById("Project Name").value + "&showTitle=true&showAuthor=true&editButton=true&pauseButton=true"
