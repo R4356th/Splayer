cd49bffc5a6f883778dae517e34d00a91742a436
R4356th <61620631+R4356th@users.noreply.github.com>
2020-08-02 16:23:16 +0600

    Fix issues with JavaScript

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 function iframeload() {
-document.getElementById("project").src="https://snap.berkeley.edu/embed?project=" + document.getElementById("Username").value + "&user="+ document.getElementById("Project Name").value + "&showTitle=true&showAuthor=true&editButton=true&pauseButton=true"
+document.getElementById("project").src="https://snap.berkeley.edu/embed?project=" + document.getElementById("Project Name").value + "&user="+ document.getElementById("Username").value + "&showTitle=true&showAuthor=true&editButton=true&pauseButton=true"
 }
