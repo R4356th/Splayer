a1b38d53334187bac054c7db458ed0b6fb60c61b
Radman <61620631+R4356th@users.noreply.github.com>
2020-08-09 19:28:51 +0600

    Tidy JS code

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+var ProjectName = document.getElementById("Project Name").value;
+var Username = document.getElementById("Username").value;
+
 function iframeload() {
-document.getElementById("project").src="https://snap.berkeley.edu/embed?project=" + document.getElementById("Project Name").value + "&user="+ document.getElementById("Username").value + "&showTitle=true&showAuthor=true&editButton=true&pauseButton=true"
+document.getElementById("project").src="https://snap.berkeley.edu/embed?project=" + ProjectName + "&user="+ Username + "&showTitle=true&showAuthor=true&editButton=true&pauseButton=true"
 }
